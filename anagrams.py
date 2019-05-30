1821549058e8257518a9b2ac671d89ee03df6557
naho atsumi <atsumi@nahonoMacBook-Pro.local>
2019-05-31 00:43:30 +0900

    3rd commit

--- anagrams.py
+++ anagrams.py
@@ -2,7 +2,6 @@
 import requests
 import itertools
 
-#global
 diclist=[]
 ScoreDic={"j":3,"k":3,"q":3,"x":3,"z":3,"c":2,"f":2,"h":2,"l":2,"m":2,"p":2,"v":2,"w":2,"y":2,"a":1,"b":1,"d":1,"e":1,"g":1,"i":1,"n":1,"o":1,"r":1,"s":1,"t":1,"u":1}
 dicsurl = 'http://icanhazwordz.appspot.com/dictionary.words'
@@ -14,19 +13,9 @@ def ReadDics(url):
     try:
         r = requests.get(url)
         diclist = sorted(r.text.split())
-        sortdic = []
-        #辞書型を作成
-        #sortdicをやめて全部セットにしてみた
         for k in diclist:
             myset.add("".join(sorted(k.lower())))
-            #sortdic.append("".join(sorted(k.lower())))
             anag_dics["".join(sorted(k.lower()))] = k
-        #print(sortdic)
-        #sortdic.sort()
-        #print(sortdic)
-        #print(len(sortdic))
-        #myset = set(sortdic)
-        #print(anag_dics)
 
         
     except requests.exceptions.RequestException as err:
@@ -34,11 +23,8 @@ def ReadDics(url):
 
 
 def Matching(wordL):
-    #print("BBBBB")
-    #print(wordL)
     answer = set(wordL) & myset
     if answer != set() :
-        #print("AAA")
         maxscore=0
         print("------answer------")
         for l in answer:
@@ -61,14 +47,14 @@ def Combi(word ,num):
     for i in list(itertools.combinations(word, len(word)-num)):
         combiwordlist.append("".join(i))
     return combiwordlist
-    #print(combiwordlist)
 
 #main
 #辞書を開く
 ReadDics(dicsurl)
 #print(myset)       #辞書が開いているか確認
-#標準入力から単語を受け取る
+
 while(1):
+    #標準入力から単語を受け取る
     print("--INPUT--")
     str=input()
     if str == "QQQ":
@@ -85,5 +71,5 @@ while(1):
 
 
 
-#表示
+#終了
 print("FINISH")
